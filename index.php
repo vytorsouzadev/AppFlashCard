6adc7de9f7d1d37d608719bd8f9a9dbdd71ec17f
VITOR SOUZA <vytorsouzadev@gmail.com>
2022-02-15 18:06:38 -0300

    add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> App Flash Card</title>
+</head>
+<body>
+    <h1>lista de projetos</h1>
+    <li><a href="/portifolio">Voltar Para Home</a></li>
+</body>
+</html>
